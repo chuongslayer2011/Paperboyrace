011016d3585fa963dcca6ed772a700a2f80d0779
chuongslayer2011 <nguyentruong22112003@gmail.com>
2024-11-27 15:08:30 +0700

    fix physical material village map

--- Assets/_UI/Resources/UI/Scripts/Win.cs
+++ Assets/_UI/Resources/UI/Scripts/Win.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Win : UICanvas
 {
-    public TMP_Text coinObtain;
+    public Text coinObtain;
+    public Text processText;
+    public Image imageFillBar;
+    public Image imageEmtyBar;
+    public Sprite townSprite;
+    public Sprite villageSprite;
+    public Text deliveryMailQuantityCurrentLevelText;
+    private int deliveryMailQuantityCurrentLevel;
+    
     private void OnEnable()
     {
         coinObtain.text = MapManager.Ins.GetObtainCoinOnCurrentMap().ToString();
+        StartCoroutine(ObtainMail());
     }
-    public void MainMenuButton()
+    public void CollectButton()
     {
         Close(0);
         GameManager.Ins.Loading();
@@ -21,5 +31,22 @@ public class Win : UICanvas
         Close(0);
 
 
+    }
+    private IEnumerator ObtainMail()
+    {
+        deliveryMailQuantityCurrentLevel = MapManager.Ins.GetDeliveriedMailQuantityOnCurrentLevel();
+        imageFillBar.fillAmount = MapManager.Ins.GetDeliveriedMailQuantityOnCurrentZone() / 10;
+        yield return new WaitForSeconds(1f);
+        processText.text = MapManager.Ins.GetDeliveriedMailQuantityOnCurrentZone().ToString() + "/10";
+        while (deliveryMailQuantityCurrentLevel > 0)
+        {
+            yield return new WaitForSeconds(1f);
+            deliveryMailQuantityCurrentLevel -= 1;
+            deliveryMailQuantityCurrentLevelText.text = deliveryMailQuantityCurrentLevel.ToString();            
+            MapManager.Ins.UpdateDeliveriedMailQuantityOnCurrentZone();
+            processText.text = MapManager.Ins.GetDeliveriedMailQuantityOnCurrentZone().ToString() + "/10";
+            imageFillBar.fillAmount = MapManager.Ins.GetDeliveriedMailQuantityOnCurrentZone() / 10;
+
+        }
     }
 }
